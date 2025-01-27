8322b4b770866fa0f1c9f300c00904ca2db6221b
Ayuzerak <60190839+Ayuzerak@users.noreply.github.com>
2025-01-27 01:25:20 +0100

    Update update.py

--- update.py
+++ update.py
@@ -583,9 +583,13 @@ def ping_server(server: str, timeout=10, retries=1, backoff_factor=2, verify_ssl
     if not server.startswith(("http://", "https://")):
         server = "https://" + server
 
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+
     for attempt in range(1, retries + 1):
         try:
-            response = requests.get(server, timeout=timeout, verify=verify_ssl)
+            response = requests.get(server, headers=headers, timeout=timeout, verify=verify_ssl)
             if response.status_code == 200:
                 VSlog(f"Ping succeeded for {server}. Status code: {response.status_code}")
                 return True
