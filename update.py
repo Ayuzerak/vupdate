de15e7e8f1ef533b843a9a9826bf269e68890f10
Ayuzerak <60190839+Ayuzerak@users.noreply.github.com>
2025-01-31 22:08:56 +0100

    Update update.py

--- update.py
+++ update.py
@@ -1258,9 +1258,7 @@ def get_livetv_url():
     """Récupère l'URL actuelle de LiveTV depuis son site référent."""
     VSlog("Récupération de l'URL de LiveTV.")
     try:
-        response = requests.get("https://www.vpnclub.fr/livetv-nouvelle-adresse/", headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-        }, timeout=10)
+        response = requests.get("https://www.vpnclub.fr/livetv-nouvelle-adresse/", timeout=10)
         content = response.text
         
         # Trouver la position du texte clé
